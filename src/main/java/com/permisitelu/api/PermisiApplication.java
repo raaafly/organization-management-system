5f859bf60aff158b35451702e38b12e6d6c63a69
M Rafly Rafsanjani <muh.raflyrafsanjani@gmail.com>
2022-09-28 17:34:59 +0700

    initial commit

---
+++ src/main/java/com/permisitelu/api/PermisiApplication.java
@@ -0,0 +1,13 @@
+package com.permisitelu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermisiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermisiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/permisitelu/api/PermisiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.permisitelu.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermisiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
