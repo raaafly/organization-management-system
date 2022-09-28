24bdfde8c34ffa17baa00a4bced9a89eef0a669a
M Rafly Rafsanjani <muh.raflyrafsanjani@gmail.com>
2022-09-28 17:46:41 +0700

    entry point user authentication

---
+++ src/main/java/com/permisitelu/api/security/UserAuthenticationException.java
@@ -0,0 +1,28 @@
+package com.permisitelu.api.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.permisitelu.api.common.ExceptionMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class UserAuthenticationException implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(),
+                new ExceptionMessage(authException.getMessage(), HttpStatus.UNAUTHORIZED));
+    }
+}
