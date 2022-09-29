ec86907885f6e2d2f94e59e6c1081dbe7a715dc5
M Rafly Rafsanjani <muh.raflyrafsanjani@gmail.com>
2022-09-29 10:03:59 +0700

    add initial data roles

--- src/main/java/com/permisitelu/api/config/AppConfiguration.java
+++ src/main/java/com/permisitelu/api/config/AppConfiguration.java
@@ -4,6 +4,8 @@ import com.permisitelu.api.module.faculty.Faculty;
 import com.permisitelu.api.module.faculty.FacultyRepository;
 import com.permisitelu.api.module.major.Major;
 import com.permisitelu.api.module.major.MajorRepository;
+import com.permisitelu.api.module.role.Role;
+import com.permisitelu.api.module.role.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
@@ -21,11 +23,21 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class AppConfiguration {
     private final MajorRepository majorRepository;
     private final FacultyRepository facultyRepository;
+    private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Bean
     CommandLineRunner runner() {
         return args -> {
+            Role SA = new Role(null, "SUPER ADMIN");
+            Role ADMIN = new Role(null, "ADMINISTRATOR");
+            Role MODERATOR = new Role(null, "MODERATOR");
+            Role MEMBER = new Role(null, "MEMBER");
+            roleRepository.save(SA);
+            roleRepository.save(ADMIN);
+            roleRepository.save(MODERATOR);
+            roleRepository.save(MEMBER);
+
             Faculty FTE = new Faculty("Teknik Elektro");
             Faculty FIF = new Faculty("Informatika");
             Faculty FRI = new Faculty("Rekayasa Industri");
