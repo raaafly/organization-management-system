a8522b61eb2321fdaa2672e519bef63ba372157f
M Rafly Rafsanjani <muh.raflyrafsanjani@gmail.com>
2022-09-29 17:16:55 +0700

    add: new module positions entity, dto, service, repository, controller

---
+++ src/main/java/com/permisitelu/api/module/position/Position.java
@@ -0,0 +1,23 @@
+package com.permisitelu.api.module.position;
+
+import com.permisitelu.api.module.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "Positions")
+public class Position extends BaseEntity implements Serializable {
+    @Column(name = "position_name", length = 100)
+    private String name;
+}
