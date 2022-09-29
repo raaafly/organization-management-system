5e7f854b4835b936214c4d17b49f74191dd0997f
M Rafly Rafsanjani <muh.raflyrafsanjani@gmail.com>
2022-09-29 17:14:20 +0700

    modify: remove unusable import

--- src/main/java/com/permisitelu/api/module/major/MajorRepository.java
+++ src/main/java/com/permisitelu/api/module/major/MajorRepository.java
@@ -3,8 +3,6 @@ package com.permisitelu.api.module.major;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface MajorRepository extends JpaRepository<Major, Long> {
     boolean existsByNameIgnoreCase(String name);
