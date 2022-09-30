9bf4281efa8390cb3513cfd8b8b93bfb3474a828
M Rafly Rafsanjani <muh.raflyrafsanjani@gmail.com>
2022-09-30 13:35:52 +0700

    delete unused file

--- src/main/java/com/permisitelu/api/module/position/PositionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.permisitelu.api.module.position;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PositionRepository extends JpaRepository<Position, Long> {
-    boolean existsByNameIgnoreCase(String name);
-}
