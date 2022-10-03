45c8fa2ff7e1a7b6cbfae53d3a2d7562744815b2
M Rafly Rafsanjani <muh.raflyrafsanjani@gmail.com>
2022-10-03 16:59:22 +0700

    no change anything

--- src/main/java/com/permisitelu/api/config/AuditConfiguration.java
+++ src/main/java/com/permisitelu/api/config/AuditConfiguration.java
@@ -1,12 +1,12 @@
 package com.permisitelu.api.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component
-public class AuditableConfiguration implements AuditorAware<String> {
+@Configuration
+public class AuditConfiguration implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
         return Optional.of("SUPER ADMIN");
