aef3917c3a902394ab6d0c744e78254e4169a250
M Rafly Rafsanjani <muh.raflyrafsanjani@gmail.com>
2022-10-03 17:06:08 +0700

    add new module Membership entity and repository

---
+++ src/main/java/com/permisitelu/api/module/Membership/MembershipRepository.java
@@ -0,0 +1,8 @@
+package com.permisitelu.api.module.Membership;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MembershipRepository extends JpaRepository<Membership, Long> {
+}
