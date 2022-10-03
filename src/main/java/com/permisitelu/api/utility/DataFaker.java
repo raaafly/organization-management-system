e1535a9417a5e3d09fa715814a2c1ea3328861ee
M Rafly Rafsanjani <muh.raflyrafsanjani@gmail.com>
2022-10-03 16:54:32 +0700

    initial data

---
+++ src/main/java/com/permisitelu/api/utility/DataFaker.java
@@ -0,0 +1,92 @@
+package com.permisitelu.api.utility;
+
+public class DataFaker {
+    public static String[] departmentFaker = {
+            "INTI",
+            "Event Organizer Creator",
+            "Human Resources Department",
+            "Financial Sponsorship Department",
+            "Main Branch Institute",
+            "Information and Communication Department"
+    };
+
+    public static String[] roleFaker = {
+            "SUPER ADMIN",
+            "ADMINISTRATOR",
+            "MODERATOR",
+            "MEMBER"
+    };
+
+    public static String[] facultyFaker = {
+            "Teknik Elektro",
+            "Rekayasa Industri",
+            "Informatika",
+            "Ekonomi dan Bisnis",
+            "Komunikasi dan Bisnis",
+            "Industri Kreatif",
+            "Ilmu Terapan"
+    };
+
+    public static String[] majorFTE = {
+            "Teknik Elektro",
+            "Teknik Telekomunikasi",
+            "Teknik Komputer",
+            "Teknik Fisika",
+            "Teknik Sistem Energi",
+            "Teknik Biomedis"
+    };
+
+    public static String[] majorFRI = {
+            "Teknik Industri",
+            "Sistem Informasi",
+            "Teknik Logistik"
+    };
+
+    public static String[] majorFIF = {
+            "Informatika",
+            "Teknologi Informasi",
+            "Rekayasa Perangkat Lunak",
+            "PJJ Informatika",
+            "Sains Data",
+    };
+
+    public static String[] majorFEB = {
+            "Manajemen Bisnis Telekomunikasi dan Informatika",
+            "Akuntansi",
+    };
+
+    public static String[] majorFKB = {
+            "Administrasi Bisnis",
+            "Ilmu Komunikasi",
+            "Hubungan Masyarakat Digital",
+            "Penyiaran Digital",
+    };
+
+    public static String[] majorFIK = {
+            "Desain Komunikasi Visual",
+            "Desain Produk",
+            "Desain Interior",
+            "Kriya",
+            "Seni Rupa",
+    };
+
+    public static String[] majorFIT = {
+            "Teknologi Telekomunikasi",
+            "Rekayasa Perangkat Lunak Aplikasi",
+            "Sistem Informasi",
+            "Sistem Informasi Akuntansi",
+            "Teknologi Komputer",
+            "Manajemen Pemasaran",
+            "Perhotelan",
+            "Teknologi Rekayasa Multimedia",
+    };
+
+    public static String[] positions = {
+            "Ketua Umum",
+            "Wakil Ketua Umum",
+            "Sekretaris",
+            "Bendahara",
+            "Kepala Departemen",
+            "Staff"
+    };
+}
